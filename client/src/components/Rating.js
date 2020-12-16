1b24d699206cf6820c93286c531236ec667e7971
karankumarshreds <naileditnerd@gmail.com>
2020-12-16 22:31:04 +0530

    refactor star rating component

--- client/src/components/Rating.js
+++ client/src/components/Rating.js
@@ -1,55 +1,23 @@
 import React from "react";
 
 const Rating = ({ value }) => {
+  let stars = [];
+  for (let i = 1; i <= 5; i++) {
+    stars.push(
+      <i
+        className={
+          value >= i
+            ? `fas fa-star`
+            : value >= i - 0.5
+            ? `fas fa-star-half`
+            : `far fa-star`
+        }
+      />
+    );
+  }
   return (
     <div className="Rating">
-      <span>
-        <i
-          className={
-            value >= 1
-              ? `fas fa-star`
-              : value >= 1 - 0.5
-              ? `fas fa-star-half`
-              : `far fa-star`
-          }
-        />
-        <i
-          className={
-            value >= 2
-              ? `fas fa-star`
-              : value >= 2 - 0.5
-              ? `fas fa-star-half`
-              : `far fa-star`
-          }
-        />
-        <i
-          className={
-            value >= 3
-              ? `fas fa-star`
-              : value >= 3 - 0.5
-              ? `fas fa-star-half`
-              : `far fa-star`
-          }
-        />
-        <i
-          className={
-            value >= 4
-              ? `fas fa-star`
-              : value >= 4 - 0.5
-              ? `fas fa-star-half`
-              : `far fa-star`
-          }
-        />
-        <i
-          className={
-            value >= 5
-              ? `fas fa-star`
-              : value >= 5 - 0.5
-              ? `fas fa-star-half`
-              : `far fa-star`
-          }
-        />
-      </span>
+      <span>{stars}</span>
     </div>
   );
 };
